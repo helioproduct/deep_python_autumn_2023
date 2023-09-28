bcc4bf8e9634c7b64b3a5d5468c7cb961773b914
Nikolay Popov <helioproduct.sync@gmail.com>
2023-09-28 22:40:47 +0300

    templeate added

---
+++ 01/main.py
@@ -0,0 +1,24 @@
+class TicTacGame:
+
+    def __init__(self):
+        pass
+
+    def show_board():
+        pass
+
+    def validate_input():
+        pass
+
+    def start_game(self):
+        pass
+
+    def check_winner():
+        pass
+
+
+if __name__ == "__main__":
+    game = TicTacGame()
+    game.start_game()
+
+
+
