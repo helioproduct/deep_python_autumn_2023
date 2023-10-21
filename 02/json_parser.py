b2af21a86250ee4110f51c0d11c58338a62a339b
Nikolay Popov <helioproduct.sync@gmail.com>
2023-10-21 23:55:55 +0300

    added decorator for mean time of last 5 function calls

--- 02/json_parser.py
+++ 02/json_parser.py
@@ -1,4 +1,24 @@
 import json
+import time
 
-def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=None):
-    pass
+
+def mean(k):
+    execution_times = []
+
+    def timer(func):
+        def wrapper(*args, **kwargs):
+            start_ts = time.time()
+            res = func(*args, **kwargs)
+            end_ts = time.time()
+
+            execution_times.append(end_ts - start_ts)
+            if len(execution_times) > k:
+                execution_times.pop(0)
+
+            average_time = sum(execution_times) / len(execution_times)
+            print(f"mean time of last {len(execution_times)} calls = {average_time}")
+            return res
+
+        return wrapper
+
+    return timer
