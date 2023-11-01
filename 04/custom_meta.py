6a9301d2bcab6404b65942dc6a32b3a347a37740
Nikolay Popov <helioproduct.sync@gmail.com>
2023-11-01 23:30:35 +0300

    code cleaned

--- 04/custom_meta.py
+++ 04/custom_meta.py
@@ -14,32 +14,3 @@ class CustomMeta(type):
         new_dct["__setattr__"] = custom_set
 
         return super().__new__(cls, name, bases, new_dct)
-
-
-class CustomClass(metaclass=CustomMeta):
-    x = 50
-
-    def __init__(self, val=99):
-        self.val = val
-
-    def line(self):
-        return 100
-
-    def __str__(self):
-        return "Custom_by_metaclass"
-
-
-assert CustomClass.custom_x == 50
-# CustomClass.x  # ошибка
-inst = CustomClass()
-assert inst.custom_x == 50
-assert inst.custom_val == 99
-assert inst.custom_line() == 100
-assert str(inst) == "Custom_by_metaclass"
-# inst.x  # ошибка
-# inst.val  # ошибка
-# inst.line()  # ошибка
-# inst.yyy  # ошибка
-inst.dynamic = "added later"
-assert inst.custom_dynamic == "added later"
-# inst.dynamic  # ошибка
