23326718e7fbf5999cd98447e5053d9b8ecd37db
Nikolay Popov <helioproduct.sync@gmail.com>
2023-11-02 00:27:52 +0300

    structure added

---
+++ 04/descriptor.py
@@ -0,0 +1,23 @@
+class PhoneNumber:
+    pass
+
+
+class Email:
+    pass
+
+
+class PositiveInteger:
+    pass
+
+
+class PersonName:
+    pass
+
+
+class User:
+    num = PhoneNumber()
+    email = Email()
+    price = PersonName()
+
+    def __init__(self):
+        pass
