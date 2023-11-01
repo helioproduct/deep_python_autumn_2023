6222ee91d5616312f122c84cdeec8235df7e7b00
Nikolay Popov <helioproduct.sync@gmail.com>
2023-11-02 00:12:42 +0300

    class fields assignment without custom_prefix fixed

--- 04/custom_meta.py
+++ 04/custom_meta.py
@@ -1,4 +1,9 @@
 class CustomMeta(type):
+    def __setattr__(cls, attribute, value):
+        if not attribute.startswith("__"):
+            attribute = "custom_" + attribute
+        super().__setattr__(attribute, value)
+
     def __new__(cls, name, bases, dct):
         def custom_set(object, attribute, value):
             if not attribute.startswith("__"):
@@ -11,6 +16,21 @@ class CustomMeta(type):
             if not key.startswith("__"):
                 new_key = "custom_" + key
             new_dct[new_key] = dct[key]
-        new_dct["__setattr__"] = custom_set
 
-        return super().__new__(cls, name, bases, new_dct)
+        new_dct["__setattr__"] = custom_set
+        new_class = super().__new__(cls, name, bases, new_dct)
+
+        return new_class
+
+
+class CustomClass(metaclass=CustomMeta):
+    x = 50
+
+    def __init__(self, val=99):
+        self.val = val
+
+    def test_func(self) -> str:
+        return f"{self}100"
+
+    def __str__(self):
+        return "Custom_by_metaclass"
