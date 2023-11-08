13b05d4b1a272f60834dc53b380236aea0e2859c
Nikolay Popov <helioproduct.sync@gmail.com>
2023-11-09 01:09:41 +0300

    made insert and remove private

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -13,33 +13,33 @@ class LRUCache:
         self.left.next, self.right.prev = self.right, self.left
 
     # remove node from list
-    def remove(self, node):
+    def __remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
     # insert node at right
-    def insert(self, node):
+    def __insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
         node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
+            self.__remove(self.cache[key])
+            self.__insert(self.cache[key])
             return self.cache[key].val
         return None
 
     def set(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.__remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        self.__insert(self.cache[key])
 
         if len(self.cache) > self.cap:
             # remove from the list and delete the LRU from hashmap
             lru = self.left.next
-            self.remove(lru)
+            self.__remove(lru)
             del self.cache[lru.key]
 
 
